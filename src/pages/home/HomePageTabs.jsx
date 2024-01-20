3bbf03d06073b915842dc93adf9897958fcfebc5
Arahas Technologies <it.github@arahas.com>
2024-01-20 10:38:53 +0530

    Changes 20 Jan-10:30AM

--- src/pages/home/HomePageTabs.jsx
+++ src/pages/home/HomePageTabs.jsx
@@ -43,7 +43,7 @@ function HomePageTabs() {
                 }
                 key={item.label}
               >
-                <Badge.Ribbon placement="start" text="VCSI: 74">
+                <Badge.Ribbon placement="start" text="VCSI: 70.3">
                   <Card>{item.children}</Card>
                 </Badge.Ribbon>
               </TabPane>

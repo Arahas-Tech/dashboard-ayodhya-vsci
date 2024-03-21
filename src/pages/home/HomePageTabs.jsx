b051e422778acc0eec2a6d633eef979c9e165f5b
Arahas Technologies <it.github@arahas.com>
2024-03-21 12:08:35 +0530

    Fix: Spelling Fix

--- src/pages/home/HomePageTabs.jsx
+++ src/pages/home/HomePageTabs.jsx
@@ -27,7 +27,7 @@ function HomePageTabs() {
       <Layout>
         <Header className={styles.header}>
           <img src={ADALogo} alt="ada-logo" className={styles.logo} />
-          <h2 className="header-text">Vedic City Sustainablility Index</h2>
+          <h2 className="header-text">Vedic City Sustainability Index</h2>
           <img src={ArahasLogo} alt="arahas-logo" height="40" />
         </Header>
 
